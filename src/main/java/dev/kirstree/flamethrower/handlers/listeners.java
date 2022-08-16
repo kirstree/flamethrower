1b059d94dac81583843c3a749e5e24d5ef81d979
Kirstree <kirstree@admin.me>
2022-08-16 13:20:07 +0100

    + crimson particle assigned

--- src/main/java/dev/kirstree/flamethrower/handlers/listeners.java
+++ src/main/java/dev/kirstree/flamethrower/handlers/listeners.java
@@ -1,22 +1,28 @@
 package dev.kirstree.flamethrower.handlers;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.Particle;
 import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.util.Vector;
 
 import java.util.ArrayList;
 
 public class listeners implements Listener {
 
     @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent e){
+    public void onPlayerJoin(PlayerJoinEvent e) {
         Player p = e.getPlayer();
 
         ItemStack flamethrower = new ItemStack(Material.DIAMOND_HOE, 1);
@@ -25,7 +31,7 @@ public class listeners implements Listener {
         ItemMeta meta = flamethrower.getItemMeta();
         assert meta != null;
         meta.setDisplayName(ChatColor.DARK_RED + ChatColor.BOLD.toString() + "FLAMETHROWER");
-        meta.addEnchant(Enchantment.DAMAGE_ALL, 10, false);
+        meta.addEnchant(Enchantment.DAMAGE_ALL, 50, false);
 
         ArrayList<String> lore = new ArrayList<>();
         lore.add(ChatColor.RED + "Shoot fire!");
@@ -35,8 +41,21 @@ public class listeners implements Listener {
 
         flamethrower.setItemMeta(meta);
 
-        inv.setItem(1, flamethrower);
+        inv.setItem(0, flamethrower);
+    }
 
+    @EventHandler
+    public void interactEvent(PlayerInteractEvent e) {
+        Player p = e.getPlayer();
 
+        if(p.getInventory().getItemInMainHand().getType() == Material.DIAMOND_HOE &&
+                e.getAction().equals(Action.LEFT_CLICK_BLOCK)){
+
+            Particle part = Particle.CRIMSON_SPORE;
+            Location loc = p.getLocation();
+            Vector direction = loc.getDirection();
+
+            p.getWorld().spawnParticle();
+        }
     }
 }
