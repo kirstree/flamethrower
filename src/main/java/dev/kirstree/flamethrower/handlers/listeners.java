4affa26f190dc66e233fee9ac7719e4d83850dc4
outcastjackalyn <kaos@thegame.com>
2022-08-18 17:23:12 +0100

    apparently this wasn't staged with last commit sorry

--- src/main/java/dev/kirstree/flamethrower/handlers/listeners.java
+++ src/main/java/dev/kirstree/flamethrower/handlers/listeners.java
@@ -1,10 +1,7 @@
 package dev.kirstree.flamethrower.handlers;
 
 import dev.kirstree.flamethrower.Flamethrower;
-import org.bukkit.ChatColor;
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.Particle;
+import org.bukkit.*;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -19,6 +16,7 @@ import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.Vector;
 
 import java.util.ArrayList;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class listeners implements Listener {
 
@@ -61,31 +59,41 @@ public class listeners implements Listener {
         }
     }
 
-    public void shootParticle(Player p, Particle particle, double velocity) {
+    public void shootParticle(Player p, Particle particle, double velocity, int count, double maxSpread) {
 
-            Location loc = p.getEyeLocation();
+            Location loc = p.getEyeLocation().add(0d, -0.14d, 0d);
             Vector direction = loc.getDirection();
-            double distance = 1d;
+            //it might technically be more efficient to use getters before the loop as the values are the same
+            for(int i = 0; i< count; i++){
 
-            p.getWorld().spawnParticle(particle, loc, 0, direction.getX()*distance, direction.getY()*distance,
-                    direction.getZ()*distance, velocity);
+                p.getWorld().spawnParticle(particle, loc, 0, direction.getX() + randomOffset(maxSpread),
+                        direction.getY() + randomOffset(maxSpread),
+                        direction.getZ() + randomOffset(maxSpread), velocity);
+            }
 
     }
+    public double randomOffset(double maxOffset){
+        return ThreadLocalRandom.current().nextDouble(-maxOffset, maxOffset);
+    }
 
-    private final long durationMillis = 5000;
+    private final long durationMillis = 420;
 
     public void burn(Player p) {
+        // might be nice to add a check so that only one runnable can be running per player at a time - not sure if needed tho
+        // would be cool also to add an invisible projectile to deal damage
         new BukkitRunnable() {
             final long startMilli = System.currentTimeMillis();
 
             public void run() {
                 long difference = ((startMilli + durationMillis) - System.currentTimeMillis());
 
-                shootParticle(p, Particle.FLAME, 1.5);
+                shootParticle(p, Particle.FLAME, 0.8d, 6, 0.1d);
+                shootParticle(p, Particle.SMALL_FLAME, 0.8d, 4, 0.05d);
+                shootParticle(p, Particle.SMOKE_NORMAL, 0.8d, 5, 0.12d);
                 if (difference < 0) {
                     cancel();
                 }
             }
-        }.runTaskTimer(Flamethrower.getInstance(), 0, 3);
+        }.runTaskTimer(Flamethrower.getInstance(), 0, 1);
     }
 }
